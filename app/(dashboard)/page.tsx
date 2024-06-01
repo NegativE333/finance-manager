4aa45387a3387d4c8673b5363a764b3692d1bee6
Om Tekade <omtekade914@gmail.com>
2024-06-01 20:04:36 +0530

    hono setup done

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,10 +1,9 @@
-import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
+
   return (
     <div>
-      Hi there
-      <UserButton afterSignOutUrl="/"/>
+      
     </div>
   );
 }
