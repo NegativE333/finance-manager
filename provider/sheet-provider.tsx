d248a7e84b13db3482b591f91c182706dd3798ad
Om Tekade <omtekade914@gmail.com>
2024-06-10 12:40:26 +0530

    account delete and edit functionality add

--- provider/sheet-provider.tsx
+++ provider/sheet-provider.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { EditAccountSheet } from "@/features/accounts/components/edit-account-sheet";
 import { NewAccountSheet } from "@/features/accounts/components/new-account-sheet";
 import { useEffect, useState } from "react";
 
@@ -16,6 +17,7 @@ export const SheetProvider = () => {
     return(
         <>
             <NewAccountSheet />
+            <EditAccountSheet />
         </>
     )
 }
