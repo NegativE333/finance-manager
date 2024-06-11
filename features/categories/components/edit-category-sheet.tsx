41229ac9c48d1adea2e41fc7d258825a2b6ec501
Om Tekade <omtekade914@gmail.com>
2024-06-11 12:13:53 +0530

    categories add delete and update functionality add

---
+++ features/categories/components/edit-category-sheet.tsx
@@ -0,0 +1,99 @@
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+
+} from '@/components/ui/sheet';
+import { CategoryForm } from './category-form';
+import { insertCategoriesSchema } from '@/db/schema';
+import { z } from 'zod';
+import { useOpenCategory } from '../hooks/use-open-category';
+import { useGetCategory } from '../api/use-get-category';
+import { Loader } from 'lucide-react';
+import { useEditCategory } from '../api/use-edit-category';
+import { useDeleteCategory } from '../api/use-delete-category';
+import { useConfirm } from '@/hooks/use-confirm';
+
+const formSchema = insertCategoriesSchema.pick({
+    name: true,
+})
+
+type FormValues = z.input<typeof formSchema>;
+
+export const EditCategorySheet = () => {
+
+    const { isOpen, onClose, id } = useOpenCategory();
+
+    const [ConfirmDialog, confirm] = useConfirm(
+        "Are you sure?",
+        "You are about to delete this category",
+    )
+
+    const categoryQuery = useGetCategory(id);
+
+    const mutation = useEditCategory(id);
+    const deleteMutation = useDeleteCategory(id);
+
+    const isPending = mutation.isPending || deleteMutation.isPending;
+
+    const isLoading = categoryQuery.isLoading;
+
+    const onSubmit = (values: FormValues) => {
+        mutation.mutate(values, {
+            onSuccess: () => {
+                onClose();
+            }
+        });
+    }
+
+    const onDelete = async () => {
+        const ok = await confirm();
+
+        if (ok) {
+            deleteMutation.mutate(undefined, {
+                onSuccess: () => {
+                    onClose();
+                }
+            })
+        }
+    }
+
+    const defaultValues = categoryQuery.data ? {
+        name: categoryQuery.data.name
+    } : {
+        name: "",
+    }
+
+    return (
+        <>
+            <ConfirmDialog />
+            <Sheet open={isOpen} onOpenChange={onClose}>
+                <SheetContent className='space-y-4'>
+                    <SheetHeader>
+                        <SheetTitle>
+                            Edit Category
+                        </SheetTitle>
+                        <SheetDescription>
+                            Edit or Delete this category
+                        </SheetDescription>
+                    </SheetHeader>
+                    {isLoading ? (
+                        <div className='absolute inset-0 flex items-center justify-center'>
+                            <Loader className='size-4 text-muted-foreground animate-spin' />
+                        </div>
+                    ) : (
+                        <CategoryForm
+                            id={id}
+                            onSubmit={onSubmit}
+                            disabled={isPending}
+                            defaultValues={defaultValues}
+                            onDelete={onDelete}
+                        />
+                    )}
+                </SheetContent>
+            </Sheet>
+        </>
+    )
+}
